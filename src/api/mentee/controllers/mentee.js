74889a057cbe933658aedce552e74b304ffd3065
Joel Dacuma <joel.dacuma@carepage.dev>
2022-08-07 09:57:52 +0800

    remove mentees api

--- src/api/mentee/controllers/mentee.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  mentee controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::mentee.mentee');
--- src/api/mentee/services/mentee.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * mentee service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::mentee.mentee');
