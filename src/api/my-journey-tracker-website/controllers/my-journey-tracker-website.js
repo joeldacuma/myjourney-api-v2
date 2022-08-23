98f4325b27ad7792f7ca10e0666f7f7a9d3dfea9
Joel Dacuma <joeldacuma1@gmail.com>
2022-08-23 18:48:42 +0800

    added my journey website api

---
+++ src/api/my-journey-tracker-website/controllers/my-journey-tracker-website.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  my-journey-tracker-website controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::my-journey-tracker-website.my-journey-tracker-website');
