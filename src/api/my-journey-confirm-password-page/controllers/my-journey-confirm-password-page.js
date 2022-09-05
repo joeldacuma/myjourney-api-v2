38bcdc0a2108559707a22bba3edb4d1cfce0f7dd
Joel Dacuma <joeldacuma1@gmail.com>
2022-09-05 20:06:04 +0800

    added confirm pages

---
+++ src/api/my-journey-confirm-password-page/controllers/my-journey-confirm-password-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  my-journey-confirm-password-page controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::my-journey-confirm-password-page.my-journey-confirm-password-page');
---
+++ src/api/my-journey-confirm-password-page/routes/my-journey-confirm-password-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * my-journey-confirm-password-page router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::my-journey-confirm-password-page.my-journey-confirm-password-page');
