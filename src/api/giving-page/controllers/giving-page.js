59d7e28c14a91c7f2d28ce75d9bd125207bdc5e1
Joel Dacuma <joeldacuma1@gmail.com>
2022-10-28 23:10:34 +0800

    added groups and giving page api

---
+++ src/api/giving-page/controllers/giving-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * giving-page controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::giving-page.giving-page');
