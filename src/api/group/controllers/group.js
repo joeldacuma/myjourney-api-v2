96bbb58ffd4b5f029a169e993570e480405d72be
Joel Dacuma <joeldacuma1@gmail.com>
2023-01-18 15:04:31 +0800

    Update patch version and groups api logic

--- src/api/group/controllers/group.js
+++ src/api/group/controllers/group.js
@@ -6,4 +6,20 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::group.group');
+module.exports = createCoreController('api::group.group', ({ strapi }) => ({
+  async update(ctx) {
+    const { body } = ctx.request;
+    const { data } = body;
+
+    if (!ctx.state.user) {
+      return ctx.unauthorized();
+    }
+
+    if (ctx.state.user.id !== data.owner) {
+        return ctx.unauthorized();
+    }
+
+    const response = await super.update(ctx);
+    return response;
+  }
+}));
